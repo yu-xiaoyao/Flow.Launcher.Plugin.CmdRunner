6366c74c20adca3f04e1ff4dcd507a0fc93d7a30
yu-xiaoyao <zxyfuncy@gmail.com>
2024-09-08 14:31:33 +0800

    add Context Menu action

--- Flow.Launcher.Plugin.CmdRunner/SettingsControlPanel.xaml.cs
+++ Flow.Launcher.Plugin.CmdRunner/SettingsControlPanel.xaml.cs
@@ -22,7 +22,85 @@ public partial class SettingsControlPanel : UserControl
     {
         CmdDataGrid.IsReadOnly = true;
         CmdDataGrid.ItemsSource = _settings.Commands;
+
+        AddContextMenu();
     }
+
+
+    private void AddContextMenu()
+    {
+        var updateItem = new MenuItem
+        {
+            Header = "更新(Update)",
+        };
+        updateItem.Click += (o, args) =>
+        {
+            if (CmdDataGrid.SelectedIndex == -1) return;
+            var item = CmdDataGrid.SelectedItem;
+            if (item is Command command)
+            {
+                _openUpdateDialogAndUpdate(command, CmdDataGrid.SelectedIndex);
+            }
+        };
+        var deleteItem = new MenuItem
+        {
+            Header = "删除(Delete)",
+        };
+        deleteItem.Click += (o, args) =>
+        {
+            if (CmdDataGrid.SelectedIndex == -1) return;
+            var item = CmdDataGrid.SelectedItem;
+            if (item is Command param)
+            {
+                _settings.Commands.RemoveAt(CmdDataGrid.SelectedIndex);
+            }
+        };
+
+        var moveUp = new MenuItem
+        {
+            Header = "向上移动(Move Up)",
+        };
+        moveUp.Click += (o, args) =>
+        {
+            var index = CmdDataGrid.SelectedIndex;
+            if (index <= 0) return;
+
+            var cur = _settings.Commands[index];
+            var pre = _settings.Commands[index - 1];
+
+            _settings.Commands[index] = pre;
+            _settings.Commands[index - 1] = cur;
+        };
+        var moveDown = new MenuItem
+        {
+            Header = "向下移动(Move Down)",
+        };
+        moveDown.Click += (o, args) =>
+        {
+            var index = CmdDataGrid.SelectedIndex;
+            if (index < _settings.Commands.Count - 1)
+            {
+                var cur = _settings.Commands[index];
+                var next = _settings.Commands[index + 1];
+
+                _settings.Commands[index] = next;
+                _settings.Commands[index + 1] = cur;
+            }
+        };
+
+        CmdDataGrid.ContextMenu = new ContextMenu
+        {
+            Items =
+            {
+                updateItem,
+                deleteItem,
+                moveUp,
+                moveDown,
+            },
+            StaysOpen = true
+        };
+    }
+
 
     private void Save_Settings(object sender, RoutedEventArgs e)
     {
@@ -58,10 +136,15 @@ public partial class SettingsControlPanel : UserControl
         var item = dg.SelectedItem;
         if (item is not Command command) return;
 
+        _openUpdateDialogAndUpdate(command, index);
+    }
+
+    private void _openUpdateDialogAndUpdate(Command command, int index)
+    {
         var totpAdd = new CmdRunnerWindows((newCmd, oldIndex) =>
             {
                 if (oldIndex == -1) return;
-                
+
                 newCmd.ResolveArgumentNames();
                 _settings.Commands[oldIndex] = newCmd;
             },
